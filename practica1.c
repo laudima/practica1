ddb74634afec861aa1cac5ad45905d8d47dc466b
laudima <lr255029@teradata.com>
2025-03-08 11:34:27 -0600

    first commit

---
+++ practica1.c
@@ -0,0 +1,118 @@
+#include <stdio.h> 
+#include <mpi.h>
+#include <stdlib.h> 
+#include <time.h>
+#include<stdbool.h>
+#include <limits.h>
+
+#define TAG_MESSAGE 1 
+#define INFINITY INT_MAX
+
+void encontrarRutaCorta(int size, int distancia[size][size]){
+    printf("Aplicamos Dijkstra para encontrar la ruta mas corta desde 0\n");
+
+    int dist[size]; // guarda las distancias mas cortas desde 0 
+    bool sptSet[size]; // el nodo esta incluido en el camino mas corto 
+
+    // Inicializar todas las distancias como infinito y sptSet[] como falso 
+    for (int i = 0; i < size; i++) {
+        dist[i] = INFINITY;
+        sptSet[i] = false;
+    }
+
+    // La distancia desde el nodo de origen es 0  
+    dist[0] = 0;
+
+    for (int count = 0; count < size - 1; count++) {
+	      
+        int min = INFINITY, u;
+
+		// Encuentra el vertice con la distancia minima
+        for (int v = 0; v < size; v++)
+            if (sptSet[v] == false && dist[v] <= min)
+                min = dist[v], u = v;
+
+        // marcamos el vertice como visitado  
+       sptSet[u] = true;
+
+        // Actualizamos la distancia de los vertices adyacentes al vertice seleccionado 
+        for (int v = 0; v < size; v++)
+			// si el vertice no esta en el camino mas corto, hay un camino de u a v y el peso de u a v es menor que el peso actual de v
+			// entonces actualizamos el peso de v
+            if (!sptSet[v] && distancia[u][v] && dist[u] != INFINITY && dist[u] + distancia[u][v] < dist[v])
+                dist[v] = dist[u] + distancia[u][v];
+    }
+
+    // Imprimimos la distancia de los nodos desde el nodo 0
+    printf("Vertice \t Distancia desde el nodo 0\n");
+    for (int i = 0; i < size; i++)
+        printf("%d \t\t %d\n", i, dist[i]);
+}
+int main(int argc, char** argv){ 
+	// Incializar open MPI  
+	int size; // el numero de nodos de nuestro programa 
+	int rank;//id unico de cada procesador
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	// variables que vamos usar para la comunicacion 
+	int * send_data = (int *)malloc(size * sizeof(int));
+	int * recv_data = (int *)malloc(size * sizeof(int));
+	MPI_Request* send_request = (MPI_Request*)malloc(size*sizeof(MPI_Request));
+	MPI_Request* recv_request = (MPI_Request*)malloc(size*sizeof(MPI_Request)); 
+
+	int retraso[size][size]; // matriz, nos da el peso, [0][2] nos va a dar el peso de 0 a 2 
+	
+
+	// Inicializar las distancias o pesos 
+	for(int i=0; i<size; i++){
+		for(int j= 0; j<size; j++){
+			if(i == j){
+				retraso[i][j] = 0; 
+			}else{
+				retraso[i][j] = INFINITY;
+			}
+		}
+	}
+
+	// definimos la topologia y mandar retrasos aleatorios todos vs todos en un rango del 0 al 100 
+	srand(time(NULL)+rank);
+	
+	// con la topologia todos vs todos podemos enviar mensajes y recibir el retraso en comunicacion (peso) de todos los nodos
+	for(int nodo=0; nodo < size; nodo++){
+		if(nodo!= rank){
+			int valor_aleatorio = (rand() % 1000) + 1; 
+			send_data[nodo] = valor_aleatorio; 
+			MPI_send(&send_data[nodo], 1, MPI_INIT,nodo, TAG_MESSAGE,MPI_COMM_WORLD,&send_request[nodo]);
+			MPI_recv(&recv_request[nodo], 1, MPI_INIT,nodo, TAG_MESSAGE,MPI_COMM_WORLD,&recv_request[nodo]);
+		}else{
+			send_request[nodo] = MPI_REQUEST_NULL;
+			recv_request[nodo] = MPI_REQUEST_NULL; 
+		}
+	}
+
+
+	// Esperar a que se recivan todos los nodos 
+	int received = 0; 
+
+	do{
+		int nodo; 
+		MPI_Waitany(size,recv_request, &nodo, MPI_STATUS_IGNORE); // received == size -1
+	}while(received < size -1)
+
+	MPI_Allgather(retraso[rank],size, MPI_INIT, retraso, size, MPI_INIT, MPI_COMM_WORLD); 
+
+	if( rank == 0 ){
+		encontrarRutaCorta(size, retraso); 
+	}
+	free(recv_request);
+	free(send_request);
+	free(recv_data);
+	free(send_data);
+
+	MPI_Finalize();
+
+	return 0;
+}
---
+++ readme.md
@@ -0,0 +1,44 @@
+# Practica 1
+
+
+**Nombre**: Laura Itzel Rodriguez Dimayuga
+
+**Nombre**: Félix Roberto Gómez Cedillo 
+			
+## Algoritmo Dijkstra 
+
+1. Se calcula la ruta con retraso de comunicación mínimo desde el nodo 0 hacia todos los demás
+
+2. Los retrasos se simularan tomando números aleatorios entre 1 y 100 
+
+3. Al final, basta con que se imprima el retraso total de la ruta desde el nodo 0 
+hasta cada nodo. Sin embargo, si también se reporta cual es la ruta. 
+
+
+## Pseudocodigo
+
+1. Inicializamos todas las distancias en $D$ con un valor infinito relativo (el nodo de inicio es 0)
+
+2. Para todo vértice $i \in C$ se estable que $P[i]=a$ 
+
+3. Se obtiene el vértice $s$ en $C$, y obtenemos el vértice con menor distancia. Se envía un mensaje al nodo correspondiente y se regresa un mensaje de de respuesta en donde se toma el tiempo y se le asigna su distancia correspondiente. De manera concurrente el nodo destino realiza el mismo procedimiento para calcular su distancia a sus nodos vecinos que no han sido visitados. 
+
+4. Se elimina de $C$ el vértice $s$ 
+
+5. Para cada arista $e$ en $E$ de longitud $l$, que une el vértice $s$ con algún otro vértice $t$ en $C$ 
+
+```
+Si l + D[s]< D[t], entonces:
+
+$D[t] := l + D[s]$ 
+$P[t] := s$ 
+```
+
+6. Se regresa al paso 4. 
+
+## Para corer la práctica  
+
+```
+gcc practica1.c
+```
+
